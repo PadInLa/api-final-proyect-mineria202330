5198a038e117fb515f44119660bc2a22c7068654
Juan David Padilla Paez <53383305+PadInLa@users.noreply.github.com>
2024-04-17 15:30:34 -0500

    Updated tester link

--- Tester.py
+++ Tester.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-url = 'http://localhost:5000/graphql'
+url = 'https://api-pf-mineriadedatos2023.onrender.com/graphql'
 headers = {'Content-Type': 'application/json'}
 query = {
     'query': '{ predictBenefit(bancarizado:"SI", discapacidad:"NO", etnia:"NINGUNO", nivelEscolaridad:"NINGUNO", genero:"Mujer", tipoPoblacion:"SISBEN", cantidadDeBeneficiarios:2, rangoEdad:57.5) }'
